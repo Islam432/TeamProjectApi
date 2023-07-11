3d5220b82d9f06151970004a7841257970f7c236
zhandos-manapov <zandosmanapov@gmail.com>
2023-07-11 11:27:39 +0600

    fix: code refactoring

---
+++ src/routes/user/toggle/toggle.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express'
+import { pool } from '../../../connection'
+import { QueryResult } from 'pg'
+import { StatusCodes } from 'http-status-codes'
+import { BadRequestError } from '../../../errors'
+
+export async function toggle(req: Request, res: Response) {
+  const { id } = req.params
+  const { is_active } = req.body
+  if(is_active === null) throw new BadRequestError('')
+  const result = await pool.query('UPDATE users SET is_active = $1 WHERE id = $2', [is_active, id])
+  res.status(StatusCodes.OK).json(result.rows)
+}
--- src/routes/user/user.route.ts
+++ src/routes/user/user.route.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import { findMany, findOne } from './user.controller'
-import { togllebar } from './Toglle/Toggle.controller'
+import { toggle } from './toggle/toggle.controller'
 
 const router = Router()
 
 router.get('/', findMany)
 router.get('/:id', findOne)
-router.post('/:id/toggle', togllebar)
+router.post('/:id/toggle', toggle)
 
 export default router
