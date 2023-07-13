c6cde822e49985fcf04b87050b529b06099bb7c4
zhandos-manapov <zandosmanapov@gmail.com>
2023-07-13 09:21:19 +0600

    fix: deleted files

--- src/routes/files/file.constants.ts
+++
@@ -1,4 +0,0 @@
-import path from 'path'
-
-export const CONTENT_ROOT_PATH = path.join(__dirname, '..', '..', '..', 'public')
-export const pattern = /(\.\.\/)/g
--- src/routes/files/middleware/replace-req-params.middleware.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-import { pattern } from '../file.constants'
-
-export function replaceRequestParams(req: Request, res: Response, next?: NextFunction) {
-  req.body.path = req.body.path && req.body.path.replace(pattern, '')
-  if (next) next()
-}
