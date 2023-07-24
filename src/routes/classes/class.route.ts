8e6f54a847c10370be6565771a2dd97c03a73517
zhandos-manapov <zandosmanapov@gmail.com>
2023-07-24 13:11:35 +0600

    feat: updated toggler

--- src/routes/classes/class.route.ts
+++ src/routes/classes/class.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { createOne, findMany, findOne } from './class.controller'
+import { toggle } from './toggle/toggle.controller'
 
 const router = Router()
 
 router.route('/').get(findMany).post(createOne)
 router.route('/:id').get(findOne)
+router.post('/:id/toggle', toggle)
 
 export default router
