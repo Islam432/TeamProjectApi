78f8391f15fc2f60d6e7dd6703f2c9dafca0588a
zhandos-manapov <zandosmanapov@gmail.com>
2023-07-25 16:21:03 +0600

    fix: tiny fix

--- src/routes/region/region.route.ts
+++ src/routes/region/region.route.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express'
-import authorize from '../../middleware/authorize.middleware'
-import { createOne, deleteRegion, findAll, findOne, updateRegion } from './region.controller'
-
+import { createOne, deleteOne, findAll, findOne, updateOne } from './region.controller'
 
 const router = Router()
 
 router.route('/').get(findAll).post(createOne)
-router.route('/:id').get(findOne).delete(deleteRegion).patch(updateRegion)
+router.route('/:id').get(findOne).delete(deleteOne).patch(updateOne)
 
 export default router
