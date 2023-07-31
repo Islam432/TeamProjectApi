dc6cc4ed9d2119f5b52f9096de4a2eaaf7bcfe8a
trixtertop <gadelovevgenij4@gmail.com>
2023-07-31 14:24:16 +0600

    fix :route office

--- src/mainRouter.ts
+++ src/mainRouter.ts
@@ -7,7 +7,7 @@ import filesRouter from './routes/files/file.route'
 import classesRouter from './routes/classes/class.route'
 import regionRoute from './routes/region/region.route'
 import cityRoute from './routes/city/city.route'
-import officeRoute from './routes/branch/branch.route'
+import branchRoute from './routes/branch/branch.route'
 import authorize from './middleware/authorize.middleware'
 import { replaceRequestParams } from './routes/files/middleware/replace-req-params.middleware'
 const mainRouter = Router()
@@ -20,6 +20,6 @@ mainRouter.use('/course', authorize, courseRouter)
 mainRouter.use('/classes', authorize, classesRouter)
 mainRouter.use('/region', authorize, regionRoute)
 mainRouter.use('/city', authorize, cityRoute)
-mainRouter.use('/branch', authorize, officeRoute)
+mainRouter.use('/branch', authorize, branchRoute)
 
 export default mainRouter
