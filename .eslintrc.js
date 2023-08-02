e020391109fa26143e9c37090b44a135bafae88c
zhandos-manapov <zandosmanapov@gmail.com>
2023-08-02 09:56:30 +0600

    fix: updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,12 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'standard-with-typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  root: true,
+  env: { browser: true, es2020: true },
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   parser: '@typescript-eslint/parser',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser',
-    project: './tsconfig.json',
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['react-refresh'],
   rules: {
-    'prettier/prettier': 'error',
-    quotes: [
-      'error',
-      'single',
-      {
-        avoidEscape: true,
-      },
-    ],
-    semi: ['error', 'never'],
-    '@typescript-eslint/quotes': [
-      'error',
-      'single',
-      {
-        avoidEscape: true,
-      },
-    ],
-    '@typescript-eslint/semi': ['error', 'never'],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-  ignorePatterns: ['.eslintrc.js'],
 }
