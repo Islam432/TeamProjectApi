825713027153780f9642e8d12b30084ae82bb3df
Islam Temirgaliev <islam2004tr@mail.ru>
2023-08-04 10:57:18 +0600

    create route

--- src/routes/level/level.route.ts
+++ src/routes/level/level.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import authorize from '../../middleware/authorize.middleware'
+
 import { findOne, findMany, createOne, deleteOne, updateOne } from './level.controller'
 
 const router = Router()
